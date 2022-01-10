d360f0fd78bab3986c917c1254e61c5fe9d38406
alumonabenaiah@gmail.com <alumonabenaiah71@gmail.com>
2022-01-10 16:45:48 +0100

    setup socket.io connection and updated auth services

--- services/soketAPI.js
+++ services/soketAPI.js
@@ -10,6 +10,10 @@ class Socket {
     this.io = io;
   }
 
+  error(io, msg) {
+    return io.emit("error", { msg });
+  }
+
   mainSocketConnection() {
     let io = this.io;
     io.on("connection", (socket) => {
@@ -17,7 +21,47 @@ class Socket {
 
       //   check for student_ride_request
       socket.on("student_ride_request", (data) => {
-        console.log(data);
+        const { from, to, userId, role } = data;
+
+        // check if the userId and role match in database
+        let sql = `SELECT * FROM "usersTable" WHERE id=$1 AND "userRole"=$2`;
+        try {
+          db.query(sql, [userId, role], (err, results) => {
+            if (err) {
+              // emit error
+              console.log(err);
+              return this.error(io, err.message);
+            }
+
+            // check if user exist
+            if (results.rowCount === 0) {
+              return this.error(
+                io,
+                "ride request failed, user with thast id doesnt exist"
+              );
+            }
+
+            // check for drivers online
+            const status = "on";
+            const dRole = "driver";
+            const sql2 = `SELECT * FROM "usersTable" WHERE "userRole"=$1 AND "status"=$2`;
+            db.query(sql2, [dRole, status], (err, results) => {
+              if (err) {
+                // emit error
+                return this.error(io, err.message);
+              }
+
+              const rand = Math.floor(Math.random() * results.rows.length);
+              let data = results.rows[rand];
+
+              console.log(rand, data);
+
+              io.emit("available-driver", data);
+            });
+          });
+        } catch (err) {
+          console.log(err);
+        }
       });
     });
   }
